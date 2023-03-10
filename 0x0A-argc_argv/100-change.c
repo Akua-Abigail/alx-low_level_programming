bc2b5d9bd2b7cfa47b39d04ba7694211a716d22a
Akua-Abigail <abbysab66@gmail.com>
2023-03-10 09:45:19 -0800

    100-task resolved

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,13 +15,9 @@
  */
 
 int main(int argc, char *argv[])
-
 {
-
 	int num, j, result;
-
 	int coins[] = {25, 10, 5, 2, 1};
-
 
 
 	if (argc != 2)
@@ -34,46 +30,27 @@ int main(int argc, char *argv[])
 
 	}
 
-
-
 	num = atoi(argv[1]);
-
 	result = 0;
 
-
-
 	if (num < 0)
-
 	{
 
 		printf("0\n");
-
 		return (0);
 
 	}
-
-
-
 	for (j = 0; j < 5 && num >= 0; j++)
 
 	{
-
 		while (num >= coins[j])
 
 		{
-
 			result++;
-
 			num -= coins[j];
-
 		}
-
 	}
-
-
-
 	printf("%d\n", result);
-
 	return (0);
 
 }
